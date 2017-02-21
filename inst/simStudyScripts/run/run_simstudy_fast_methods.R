e31bc5d1ec1b080df03ee5bf8ff1510468d90d5c
Evan Ray <evan.l.ray@gmail.com>
2017-02-21 10:30:40 -0500

    simulation study results with time dependence level for simulation study, associated scripts

--- inst/simStudyScripts/run/run_simstudy_fast_methods.R
+++ inst/simStudyScripts/run/run_simstudy_fast_methods.R
@@ -1,9 +1,9 @@
 rm(list = ls())
 options(warn = 2, error = recover)
 
-for(obs_dist_normal in c(FALSE, TRUE)) {
-	for(time_dependence in c(FALSE, TRUE)) {
-		for(fit_method in c("RF", "normalFMM", "normalHMM")) {
+for(obs_dist_normal in c(TRUE)) {
+	for(time_dependence in c(TRUE)) {
+		for(fit_method in c("normalFMM", "normalHMM", "RF")) {
 			sim_inds <- seq_len(50)
 			
 			for(sim_ind in sim_inds) {
--- inst/simStudyScripts/run/setupRstreamSubstreamObjects.R
+++ inst/simStudyScripts/run/setupRstreamSubstreamObjects.R
@@ -1,5 +1,11 @@
 rm(list = ls())
 library("rstream")
+library("PACwithDDM")
+
+# In this file, we create files with the random number generation stream starting point
+# for each combination of data set, accelerometer location, classification variable, and
+# fit method
+save_location <- file.path(find.package("PACwithDDM"), "simStudyScripts", "rngstreams")
 
 set.seed(81861) # this was randomly generated
 
@@ -9,61 +15,31 @@ rngstream <- new("rstream.mrg32k3a", seed = sample(1:10000, 6, rep = FALSE))
 num_sims_per_scenario <- 100L
 
 # represent data generation plus all possible levels for method
-all_fit_methods <- c("data_gen", "RFCRF", "RFCRFseqbag", "parametricBoostCRF", "RF", "normalHMM", "unregularizedParametricCRF", "L2RegularizedCRF",
-	"gradientTreeBoostCRF", "baggedFeatureSubsetGradientTreeBoostCRF", "MLRHMM", "RFHMM")
+all_fit_methods <- c("data_gen", "parametricBoostCRF", "parametricBoostMLR", "RF", "normalHMM")
 
 # number of substreams used by each method per simulation index
 substreams_used <- list(data_gen = 1,
-	RFCRF = 1001,
-	RFCRFseqbag = 1001,
 	parametricBoostCRF = 1001,
+	parametricBoostMLR = 1001,
 	RF = 1,
-	normalHMM = 1,
-	unregularizedParametricCRF = 1,
-	L2RegularizedCRF = 11,
-	gradientTreeBoostCRF = expression(2 + 10 * D * 30 + 1000),
-	baggedFeatureSubsetGradientTreeBoostCRF = expression(2 + 10 * D * 30 + 1000),
-	MLRHMM = 1,
-	RFHMM = 0)
+	normalHMM = 1)
 
-D <- 50
-
-for(obs_dist_complex in c(FALSE, TRUE)) {
-for(bayes_error_rate_high in c(FALSE, TRUE)) {
-for(redundant_features_informative in c(FALSE, TRUE)) {
-for(fit_method in all_fit_methods) {
-	if(obs_dist_complex) {
-		obs_dist <- "obsDistNormal"
-	} else {
-		obs_dist <- "obsDistNonNormal"
-	}
-
-	if(bayes_error_rate_high) {
-		bayes_error <- "BayesErrorLarge"
-	} else {
-		bayes_error <- "BayesErrorSmall"
-	}
-
-	if(redundant_features_informative) {
-		redundant_features <- "redundantFeaturesInformative"
-	} else {
-		redundant_features <- "redundantFeaturesNonInformative"
-	}
-
-	for(sim_ind in seq_len(num_sims_per_scenario)) {
-		rstream.packed(rngstream) <- TRUE
-
-		stream_filename <- paste("rngstream_simStudy", obs_dist, bayes_error, redundant_features,
-			"sim", sim_ind, sep = "_")
-		save(rngstream, file = paste0("C:/Stat/HMM/HMMEnsembles/rayThesis/inst/simStudyScripts/rngstreams/", fit_method, "/", stream_filename, ".rdata"))
-
-		rstream.packed(rngstream) <- FALSE
-
-		for(i in seq_len(eval(substreams_used[[fit_method]]))) {
-			rstream.nextsubstream(rngstream)
-		}
-	} # subj
-} # fit_method
-} # redundant_features_informative
-} # bayes_error_rate_high
+for(obs_dist in c("obsDistNormal", "obsDistNonNormal")) {
+  for(time_dependence in c("timeDependence", "noTimeDependence")) {
+    for(fit_method in all_fit_methods) {
+      for(sim_ind in seq_len(num_sims_per_scenario)) {
+        rstream.packed(rngstream) <- TRUE
+        
+        stream_filename <- paste("rngstream_simStudy", obs_dist, time_dependence,
+          "sim", sim_ind, sep = "_")
+        save(rngstream, file = paste0(save_location, "/", fit_method, "/", stream_filename, ".rdata"))
+        
+        rstream.packed(rngstream) <- FALSE
+        
+        for(i in seq_len(eval(substreams_used[[fit_method]]))) {
+          rstream.nextsubstream(rngstream)
+        }
+      } # sim_ind
+    } # fit_method
+  } # time_dependence
 } # obs_dist_complex
